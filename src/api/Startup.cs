0c2c464f4b322d33a1483d3dad7986b1c7aab200
Frans Lytzen <flytzen@neworbit.co.uk>
2021-06-28 17:30:26 +0100

    Messing with ports

--- src/api/Startup.cs
+++ src/api/Startup.cs
@@ -44,7 +44,7 @@ namespace api
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "api v1"));
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
